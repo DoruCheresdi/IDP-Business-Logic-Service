87bdcc9dbd7a5242c94d26a5c0086425e0cc4226
Doru Cheresdi <51168560+DoruCheresdi@users.noreply.github.com>
2024-04-24 02:06:07 +0300

    fixed controller advice not returning json

--- src/main/java/com/alibou/security/controller/advice/MethodArgumentNotValidExceptionAdvice.java
+++ src/main/java/com/alibou/security/controller/advice/MethodArgumentNotValidExceptionAdvice.java
@@ -1,19 +1,14 @@
 package com.alibou.security.controller.advice;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class MethodArgumentNotValidExceptionAdvice {
 
     @ResponseBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String userEmailNotUniqueHandler(MethodArgumentNotValidException ex) {
-        return "Invalid request, check parameters " + ex.getMessage();
+    public MethodArgumentNotValidException userEmailNotUniqueHandler(MethodArgumentNotValidException ex) {
+        return ex;
     }
 }
