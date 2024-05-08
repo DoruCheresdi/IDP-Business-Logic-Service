0c4f639874551b0af12d0186390eb105d64cafc8
Doru Cheresdi <51168560+DoruCheresdi@users.noreply.github.com>
2024-05-09 02:48:06 +0300

    fixed crashing on startup is mysql not up

--- src/main/java/com/alibou/security/SecurityApplication.java
+++ src/main/java/com/alibou/security/SecurityApplication.java
@@ -2,11 +2,19 @@ package com.alibou.security;
 
 import com.alibou.security.auth.AuthenticationService;
 import com.alibou.security.auth.RegisterRequest;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DatabaseDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
+
+import javax.sql.DataSource;
+
+import java.util.stream.Stream;
 
 import static com.alibou.security.enums.Role.ADMIN;
 import static com.alibou.security.enums.Role.MANAGER;
@@ -17,6 +25,27 @@ public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Bean
+	public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
+		var dsv = new DatabaseStartupValidator();
+		dsv.setDataSource(dataSource);
+		dsv.setValidationQuery(DatabaseDriver.MYSQL.getValidationQuery());
+		return dsv;
+	}
+
+	@Bean
+	public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+		return bf -> {
+			// Let beans that need the database depend on the DatabaseStartupValidator
+			// like the JPA EntityManagerFactory or Flyway
+
+			String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+			Stream.of(jpa)
+					.map(bf::getBeanDefinition)
+					.forEach(it -> it.setDependsOn("databaseStartupValidator"));
+		};
 	}
 
 	@Bean
